201a691e704c303f0caf3e5b0328fcd0ceb8fbb5
Unknown <ivchiga@gmail.com>
2018-10-23 06:53:54 +0100

    ssv ut2

---
+++ ssv/ut2/actividades/clasificaciones sistemas operativos.mm
@@ -0,0 +1,92 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="clasificaciones sistemas operativos" FOLDED="false" ID="ID_12199659" CREATED="1540166134715" MODIFIED="1540166813126" BACKGROUND_COLOR="#ccffcc" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="2.0 px" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="CLOSE_DOTS">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="por n&#xfa;mero de usuarios" POSITION="right" ID="ID_316478747" CREATED="1540166156895" MODIFIED="1540166700432" BACKGROUND_COLOR="#ffcccc" STYLE="bubble">
+<edge COLOR="#ff0000"/>
+<node TEXT="monousario" ID="ID_280753288" CREATED="1540166166906" MODIFIED="1540166661978" STYLE="bubble"/>
+<node TEXT="multiusuario" ID="ID_587058505" CREATED="1540166172175" MODIFIED="1540166661978" STYLE="bubble"/>
+</node>
+<node TEXT="por el n&#xfa;mero de proceso" POSITION="right" ID="ID_863533514" CREATED="1540166180488" MODIFIED="1540166700438" BACKGROUND_COLOR="#ffcccc" STYLE="bubble">
+<edge COLOR="#0000ff"/>
+<node TEXT="monotarea" ID="ID_1906949709" CREATED="1540166217262" MODIFIED="1540166661979" STYLE="bubble"/>
+<node TEXT="multitarea" ID="ID_115969781" CREATED="1540166221504" MODIFIED="1540166661981" STYLE="bubble"/>
+</node>
+<node TEXT="Por el n&#xfa;mero de procesadores" POSITION="left" ID="ID_57327930" CREATED="1540166243896" MODIFIED="1540166700440" BACKGROUND_COLOR="#ffcccc" STYLE="bubble">
+<edge COLOR="#00ff00"/>
+<node TEXT="monoprocesador" ID="ID_126618011" CREATED="1540166256768" MODIFIED="1540166661993" STYLE="bubble"/>
+<node TEXT="multiprocesador" ID="ID_379112025" CREATED="1540166263536" MODIFIED="1540166661992" STYLE="bubble">
+<node TEXT="multiproceso sim&#xe9;trico" ID="ID_584870032" CREATED="1540166282233" MODIFIED="1540166661994" STYLE="bubble"/>
+<node TEXT="multiproceso asim&#xe9;trico" ID="ID_618045461" CREATED="1540166293640" MODIFIED="1540166661994" STYLE="bubble"/>
+</node>
+</node>
+<node TEXT="Por el tiempo de respuesta" POSITION="left" ID="ID_838795339" CREATED="1540166304026" MODIFIED="1540166700439" BACKGROUND_COLOR="#ffcccc" STYLE="bubble">
+<edge COLOR="#ff00ff"/>
+<node TEXT="tiempo real" ID="ID_1477740486" CREATED="1540166311570" MODIFIED="1540166661991" STYLE="bubble"/>
+<node TEXT="tiempo compartido" ID="ID_821280115" CREATED="1540166316696" MODIFIED="1540166661991" STYLE="bubble"/>
+</node>
+</node>
+</map>
